ceff5d5639fc784841b0a9ce383f512715cb1438
dinorvh-code <dinorvh@gmail.com>
2026-04-07 15:39:19 -0500

    Feito

--- src/app/cards/cards.ts
+++ src/app/cards/cards.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { App } from '../app';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cards',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.html',
   styleUrl: './cards.css',
 })
+
 export class Cards {
 
 tituloPrincipal = 'Why Choose Tourz';
@@ -14,28 +15,28 @@ tituloPrincipal = 'Why Choose Tourz';
 diferenciais = [
 
 {
-icone:  'assets/icone01.png',
+icone:  'assets/images/icone01.png',
 titulo: 'Ultimate flexibility',
 descricao: 'You are in control, with free cancellation and payment options to satisfy any plan or budget'
 },
 
 {
-icone: 'assets/icone02.png',
+icone: 'assets/images/icone02.png',
 titulo: 'Memorable experiences',
 descricao: 'Browse and book tours and activities so incredible, you will want to tell your friends'
 },
 
 {
-icone: 'assets/icone03.png',
+icone: 'assets/images/icone03.png',
 titulo: 'Quality at our core',
 descricao: 'High-quality standards. Millions of reviews. A tourz company.'
 },
 
 {
-icone: 'assets;icone04.png',
+icone: 'assets/images/icone04.png',
 titulo: 'Award-winning support',
 descricao: 'New price? New plan? No problem. We are here to help, 24/7.'
-}.
+}
 
 ]
 
